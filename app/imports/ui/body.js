eb97aa32f543e5e20b6b7002f05bf8c8c0ac1cd5
Douglas Felipe Bussmann Siedschlag <douglasfelipebs@hotmail.com>
2020-07-28 23:30:59 -0300

    [ADD] #busca, #insercao - Adicionado inserção de tasks e busca ordenada a partir de uma collection

--- app/imports/ui/body.js
+++ app/imports/ui/body.js
@@ -1,11 +1,30 @@
 import { Template } from 'meteor/templating';
+import { Tasks } from '../api/tasks';
 
 import './body.html';
 
 Template.body.helpers({
-  tasks: [
-    { text: 'This is task 1' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' },
-  ],
+  tasks() {
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+Template.body.events({
+  'submit .new-task': function (event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    // Clear form
+    target.text.value = '';
+  },
 });
